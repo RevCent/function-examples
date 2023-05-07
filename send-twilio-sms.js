2b6b8566fd850da2e6a29d00736243e2d7838b72
RevCent <132793768+RevCent@users.noreply.github.com>
2023-05-06 21:10:59 -0400

    Create send-twilio-sms.js

---
+++ send-twilio-sms.js
@@ -0,0 +1,43 @@
+// Description
+// In this example, a customer is sent a text message via Twilio after successfully completing a purchase. 
+// We used the sale.created.success.paid Event notation. The event.data.item_details object contains the sale details. 
+// The customer information is contained within the sale details, accessible via event.data.item_details.customer
+
+// Trigger
+// Account Event
+
+// Event
+// sale.created.success.paid
+
+// Item Type
+// Sale
+
+// Twilio account must be in live status, not trial
+
+const Twilio = global.twilio; // Accessing the "twilio" module from the global object
+
+const sale_details = event.data.item_details; // Getting the sale details
+const customer_details = event.data.item_details.customer; // Getting the customer details
+const sale_amount = sale_details.amount_gross; // Get the amount paid for the sale via amount_gross property.
+
+// process.env properties are environment variables you should have already created
+let accountSid = process.env.twilio_account_sid;
+let token = process.env.twilio_auth_token;
+let from_number = process.env.twilio_from_number;
+
+
+let to = customer_details.phone;
+
+let twilio = new Twilio(accountSid, token);
+twilio.messages.create({
+    from: from_number,
+    to: to,
+    body: 'Thank you for your $' + sale_amount + ' purchase!'
+}, function(err, result) {
+    if (err) {
+        console.log(err);
+        callback(err);
+    } else {
+        callback(null, result.sid); // Don't forget the callback()
+    }
+});
