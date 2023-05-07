e04197c9ed3ffdb81bbc76d92e75eaa690916ad0
RevCent <132793768+RevCent@users.noreply.github.com>
2023-05-06 21:29:43 -0400

    Update auto-query-ai-assistant-to-telegram.js

--- auto-query-ai-assistant-to-telegram.js
+++ auto-query-ai-assistant-to-telegram.js
@@ -1,24 +1,37 @@
-// https://t.me/botfather
-// create the bot. get the token. process.env.bot_token === new bot token
-// open web.telegram in browser
-// go to group
-// right click on the group name on the left menu
-// click 'inspect' button
-// you will see the group id in the attribute data-peer-id="-xxxxxxxxxx" or peer="-xxxxxxxxxx"
-// group chat id : -xxxxxxxxxx
+// Description
+// An Auto Query is run and the results are sent to an AI Assistant. 
+// The AI Assistant asks OpenAI: "Please provide an ASCII formatted table of the query results"
+// The AI Assistant triggers a function, with the OpenAI response.
+// The function sends a telegram message, the contents of the message consisting of the AI's response (an ASCII table)
 
-// channel chat id : -100xxxxxxxxxx
+// Trigger
+// AI Assistant
+
+// Auto Query Docs: https://kb.revcent.com/en/tools/auto-query
+// AI Docs: https://kb.revcent.com/en/tools/ai
+
+// TELEGRAM INSTRUCTIONS
+// Go to https://t.me/botfather
+// Create the bot. Get the token. Set process.env.bot_token = new bot token
+// Open web.telegram in browser
+// Go to the group you want to send messages to.
+// Right click on the group name on the left menu
+// Click 'inspect' button
+// You will see the group id in the attribute data-peer-id="-xxxxxxxxxx" or peer="-xxxxxxxxxx"
+// Chat id : -xxxxxxxxxx
 
 
 
-let chat_id = -12345678;
+let chat_id = -12345678; // From telegram instructions.
+let bot_token = process.env.bot_token; // The bot token should be an environment variable.
+let ai_response = event.data.ai_request.ai_content; // AI response from AI assistant, which then triggered the function.
 let request = global.request;
 request.post({
     headers: {
         'content-type': 'application/x-www-form-urlencoded'
     },
-    body: 'chat_id=' + chat_id + '&text=messaging from RevCent function. Here are the AI results: ' + event.data.ai_request.ai_content.toString(),
-    url: 'https://api.telegram.org/bot' + process.env.bot_token + '/sendMessage'
+    body: 'chat_id=' + chat_id + '&text=messaging from RevCent function. Here are the AI results: ' + ai_response,
+    url: 'https://api.telegram.org/bot' + bot_token + '/sendMessage'
 }, function(error, response, body) {
     callback(null, 'OK')
 });
