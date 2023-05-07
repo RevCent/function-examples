a26743edd34f73bd37d7e5d7c66c6a2dc79b640e
RevCent <132793768+RevCent@users.noreply.github.com>
2023-05-06 21:43:11 -0400

    Update flow-activecampaign-add-contact.js

--- flow-activecampaign-add-contact.js
+++ flow-activecampaign-add-contact.js
@@ -4,6 +4,8 @@
 // Trigger
 // Flow
 
+// Flow Docs: https://kb.revcent.com/en/tools/flows
+
 const request = global['request'];
 
 let active_campaign_api_token = process.env.active_campaign_api_token; // process.env properties are environment variables you should have already created
